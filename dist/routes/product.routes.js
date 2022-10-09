505b545f2440283ac0a69324f3794018cefddff1
gdariodh <diazhernandezgabriel2001@gmail.com>
2022-10-08 23:51:35 -0300

    added updated

--- dist/routes/product.routes.js
+++ dist/routes/product.routes.js
@@ -1,32 +1,58 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const product_model_1 = require("../models/product.model");
 const route = (0, express_1.Router)();
-route.get('/', (req, res) => {
+route.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const products = yield product_model_1.Product.find();
     return res.json({
-        product: 'zapatillas',
-        msj: "OK GET PRODUCT"
+        ok: true,
+        products,
     });
-});
-route.post('/', (req, res) => {
+}));
+route.post("/", (req, res) => {
     const body = req.body;
-    console.log(body);
     const product = {
         name: body.name,
         description: body.description,
         image: body.image,
-        price: body.price
+        price: body.price,
     };
-    product_model_1.Product.create(product).then(productDb => {
+    product_model_1.Product.create(product)
+        .then((productDb) => {
         return res.json({
-            product: 'zapatillas',
-            productDb
+            product: "zapatillas",
+            productDb,
         });
-    }).catch(err => {
+    })
+        .catch((err) => {
         return res.json({
-            msg: 'Creacion de producto erronea!'
+            msg: "Creacion de producto erronea!",
         });
     });
 });
+route.put("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const body = req.body;
+    const productId = req.params.id;
+    const product = {
+        name: body.name,
+        description: body.description,
+        image: body.image,
+        price: body.price,
+    };
+    yield product_model_1.Product.findByIdAndUpdate(productId, product);
+    return res.json({
+        ok: true,
+        msg: "producto actualizado"
+    });
+}));
 exports.default = route;
--- routes/product.routes.ts
+++ routes/product.routes.ts
@@ -1,41 +1,58 @@
 import { Router, Response, Request } from "express";
 import { Product } from "../models/product.model";
 
-const route = Router()
+const route = Router();
 
-route.get('/', (req:Request, res: Response) => {
+route.get("/", async (req: Request, res: Response) => {
+  const products = await Product.find();
+
   return res.json({
-    product: 'zapatillas',
-    msj :"OK GET PRODUCT"
-  })
-})
+    ok: true,
+    products,
+  });
+});
 
-
-route.post('/', (req:Request, res: Response) => {
-
+route.post("/", (req: Request, res: Response) => {
   const body = req.body;
-
-  console.log(body)
 
   const product = {
     name: body.name,
     description: body.description,
     image: body.image,
-    price: body.price
-  }
+    price: body.price,
+  };
 
-  Product.create(product).then(productDb => {
-    return res.json({
-      product: 'zapatillas',
-      productDb
+  Product.create(product)
+    .then((productDb) => {
+      return res.json({
+        product: "zapatillas",
+        productDb,
+      });
     })
-  }).catch(err => {
-    return res.json({
-     msg: 'Creacion de producto erronea!'
-    })
-  })
+    .catch((err) => {
+      return res.json({
+        msg: "Creacion de producto erronea!",
+      });
+    });
+});
 
+route.put("/:id", async (req: Request, res: Response) => {
+  const body = req.body;
+  const productId = req.params.id;
 
-})
+  const product = {
+    name: body.name,
+    description: body.description,
+    image: body.image,
+    price: body.price,
+  };
+
+   await Product.findByIdAndUpdate(productId, product);
+
+  return res.json({
+    ok: true,
+    msg: "producto actualizado"
+  });
+});
 
 export default route;
